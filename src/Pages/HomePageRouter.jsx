2ea39d9f8bc8e0252584adfd1a52f3c2ba026dcd
Tsuki-tech <tsukitech12@gmail.com>
2023-11-06 13:36:18 +0545

    deleting ourservices component

--- src/Pages/HomePageRouter.jsx
+++ src/Pages/HomePageRouter.jsx
@@ -15,7 +15,7 @@ export default function HomePageRouter() {
         </Routes>
       </Router> */}
       <Header />
-      <Client/>
+      <Client />
       {/* <Experience/> */}
       <Contact />
       <Footer />
